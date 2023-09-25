2b6d7926430df299ada5cc9081d6f5b058acb1d0
Serg <s.ustin.92@mail.ru>
2023-09-25 16:30:36 +0300

    add Messaging with Redis project

---
+++ MessagingWithRedis/src/test/java/com/example/messagingwithredis/MessagingWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagingwithredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingWithRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
