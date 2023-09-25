a2f910f0746453292323ddc6bd8e2b875ad7dec7
Serg <s.ustin.92@mail.ru>
2023-09-25 10:51:38 +0300

    add Uploading Files project

---
+++ UploadingFiles/src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploadingfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+}
